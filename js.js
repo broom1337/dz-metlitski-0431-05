71f4192d496b4a28482c710f76ffaf713b2bc4c7
broom1337 <160509766+broom1337@users.noreply.github.com>
2024-02-19 18:12:24 +0300

    dz

---
+++ js.js
@@ -0,0 +1,31 @@
+async function methodGetUsers() {
+    let xhr = new XMLHttpRequest()
+    let xhr2 = new XMLHttpRequest()
+    xhr.open('GET','https://jsonplaceholder.typicode.com/todos', true)
+    xhr2.open('GET','https://jsonplaceholder.typicode.com/users', true)
+    xhr.send()
+    xhr2.send()
+
+    xhr.onload = function(){
+        let response = JSON.parse(xhr.response)
+        
+        if (response && Array.isArray(response) && response.length > 0) {
+            xhr2.onload = function() {
+                let usersData = JSON.parse(xhr2.response)
+                response.forEach((user, index) => {
+                    let userObj = usersData.find((userData) => userData.id === user.userId)
+                    let userName = userObj.name || 'N/A'
+                    
+                    let row = '<tr>'
+                    row += '<td>'+user.id+'</td>'
+                    row += '<td>'+userName+'</td>'
+                    row += '<td>'+user.title+'</td>'
+                    row += '<td>'+user.completed+'</td>'
+                    row += '</tr>'
+                    
+                    $('table tbody').append(row)
+                })
+            }
+        }
+    }      
+}
